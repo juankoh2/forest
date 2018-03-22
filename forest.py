7b2b92b721079323e68e172460a89e8d6926f3a0
juankoh2 <kohwanjuan@yahoo.com>
2018-03-22 21:26:39 +0800

    updated forest.py

---
+++ forest.py
@@ -0,0 +1,31 @@
+from sklearn import tree
+
+import pandas as pd
+mydata= pd.read_csv('training.csv')
+training_labels = mydata.iloc[:,8]
+
+from sklearn import preprocessing
+le = preprocessing.LabelEncoder()
+label = le.fit_transform(training_labels)
+
+clf = tree.DecisionTreeClassifier(max_depth=4)
+clf = clf.fit(mydata, mydata.type)
+
+import graphviz
+dot_data = tree.export_graphviz(clf, out_file=None,
+feature_names=mydata.columns,
+class_names=["Sugi","Hinoki","mixed deciduous","non-forest land"])
+graph = graphviz.Source(dot_data)
+
+
+graph.view('Iris')
+
+
+
+print(mydata.type)
+
+
+
+import os
+
+os.system("PAUSE")
---
+++ test_iris.py
@@ -0,0 +1,30 @@
+﻿from sklearn import datasets
+iris = datasets.load_iris()
+
+print(iris)
+
+import os
+
+os.system("PAUSE")
+
+from sklearn import tree
+import pandas as pd
+dftrain = pd.read_csv('training.csv')
+#dataframe.iloc[row index, column index]:
+training_labels = dftrain.iloc[:,0]
+from sklearn import preprocessing
+le = preprocessing.LabelEncoder()
+label = le.fit_transform(training_labels)
+clf = tree.DecisionTreeClassifier(max_depth=3)
+clf = clf.fit(iris.data, iris.target)
+
+import graphviz
+dot_data = tree.export_graphviz(clf, out_file=None,
+feature_names=iris.feature_names,
+class_names=iris.target_names,
+filled=True, rounded=True,
+special_characters=True)
+graph = graphviz.Source(dot_data)
+
+
+graph.view('Iris')
